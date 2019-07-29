3e5b0ce68a2f7cb22e7972f2fa357f179bee9858
Vilmar César Pereira Júnior <vilmar.junior@edu.sc.senac.br>
2019-07-28 21:56:37 -0300

    - Inclusão de javadocs; - Inclusão de BaseDAO, ainda não utilizado.

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -2,36 +2,41 @@ package view;
 
 import java.util.Scanner;
 
+/**
+ * Classe que contém as opções do menu principal do Dr Muquirana.
+ * 
+ * @author Adriano de Melo
+ *
+ */
 public class Menu {
-
 	Scanner teclado = new Scanner(System.in);
-	
+
 	private static final int OPCAO_MENU_CADASTRO = 1;
 	private static final int OPCAO_MENU_RELATORIO = 2;
 	private static final int OPCAO_MENU_SAIR = 3;
-	
+
 	public void apresentarMenu() {
 		int opcao = this.apresentarOpcoesMenu();
-		while (opcao != OPCAO_MENU_SAIR){
-			switch(opcao){
-				case OPCAO_MENU_CADASTRO: {
-					MenuCadastro menuCadastro = new MenuCadastro();
-					menuCadastro.apresentarMenuCadastro();
-					break;
-				}
-				case OPCAO_MENU_RELATORIO: {
-					MenuRelatorio menuRelatorio = new MenuRelatorio();
-					menuRelatorio.apresentarMenuRelatorio();
-					break;
-				}
-				default: {
-					System.out.println("\nOpção Inválida");
-				}
+		while (opcao != OPCAO_MENU_SAIR) {
+			switch (opcao) {
+			case OPCAO_MENU_CADASTRO: {
+				MenuCadastro menuCadastro = new MenuCadastro();
+				menuCadastro.apresentarMenuCadastro();
+				break;
+			}
+			case OPCAO_MENU_RELATORIO: {
+				MenuRelatorio menuRelatorio = new MenuRelatorio();
+				menuRelatorio.apresentarMenuRelatorio();
+				break;
+			}
+			default: {
+				System.out.println("\nOpção Inválida");
+			}
 			}
 			opcao = this.apresentarOpcoesMenu();
 		}
 	}
-	
+
 	private int apresentarOpcoesMenu() {
 		System.out.println("Dr. Muquirana - Controle de Gastos");
 		System.out.println("\nOpções:");
